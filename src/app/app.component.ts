c20cfd8140d7acb60e85bf6886a4f32f041031b2
Victoria Isabelle Corbolin Monte <81435565+vblmqs@users.noreply.github.com>
2025-04-17 22:06:10 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,40 @@
+//soma
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { SomaComponent } from './page/soma';
 
+//subtração
+import { SubtracaoComponent } from './page/subtracao';
+
+//multiplicação
+import { MultiplicacaoComponent } from './page/multiplicacao';
+
+//divisão
+import { DivisaoComponent } from './page/divisao';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SomaComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+
+    //soma
+    SomaComponent,
+
+    //subtração
+    SubtracaoComponent,
+
+    //multiplicação
+    MultiplicacaoComponent,
+
+    //divisão
+    DivisaoComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-  export class AppComponent {
-  title = 'soma';
+export class AppComponent {
+  title = '';
 }
