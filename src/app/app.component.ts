364f7ea828a8b03991496b1df5c7c7734fc98f16
Victoria Isabelle Corbolin Monte <vblmqs@gmail.com>
2025-04-17 21:01:58 -0300

    Soma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SomaComponent } from './page/soma';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, SomaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+  export class AppComponent {
   title = 'soma';
 }
