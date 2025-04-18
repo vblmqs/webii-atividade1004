24577a8eeba4b54451a6504b68ed90bd196b5d21
Victoria Isabelle Corbolin Monte <81435565+vblmqs@users.noreply.github.com>
2025-04-17 22:03:20 -0300

    Update soma.component.spec.ts

--- src/app/page/soma/soma.component.spec.ts
+++ src/app/page/soma/soma.component.spec.ts
@@ -1,3 +1,4 @@
+//soma
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SomaComponent } from './soma.component';
@@ -13,6 +14,81 @@ describe('SomaComponent', () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(SomaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+//subtração
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SubtracaoComponent } from './subtracao.component';
+
+describe('SubtracaoComponent', () => {
+  let component: SubtracaoComponent;
+  let fixture: ComponentFixture<SubtracaoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SubtracaoComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SubtracaoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+//multiplicação
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MultiplicacaoComponent } from './multiplicacao.component';
+
+describe('MultiplicacaoComponent', () => {
+  let component: MultiplicacaoComponent;
+  let fixture: ComponentFixture<MultiplicacaoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MultiplicacaoComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MultiplicacaoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+//divisão
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DivisaoComponent } from './divisao.component';
+
+describe('DivisaoComponent', () => {
+  let component: DivisaoComponent;
+  let fixture: ComponentFixture<DivisaoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DivisaoComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DivisaoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
