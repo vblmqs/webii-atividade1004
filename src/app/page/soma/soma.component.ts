23380e6e7caa329f11c24997bba10c79060bdbc9
Victoria Isabelle Corbolin Monte <81435565+vblmqs@users.noreply.github.com>
2025-04-17 22:04:12 -0300

    Update soma.component.ts

--- src/app/page/soma/soma.component.ts
+++ src/app/page/soma/soma.component.ts
@@ -1,3 +1,4 @@
+//soma
 import { Component } from '@angular/core';
 import { SomaService } from '../../services';
 
@@ -19,10 +20,97 @@ export class SomaComponent {
     n1 = parseFloat(numero1);
     n2 = parseFloat(numero2);
     this.res = this.somaService.somar(n1, n2);
-    }
+  }
 
-    get resultado(): string {
-      return this.res.toString();
-      }
-
+  get resultado(): string {
+    return this.res.toString();
+  }
 }
+
+//subtração
+import { Component } from '@angular/core';
+import { SubtracaoService } from '../../services';
+
+@Component({
+  selector: 'app-subtracao',
+  standalone: true,
+  imports: [],
+  templateUrl: './subtracao.component.html',
+  styleUrl: './subtracao.component.css'
+})
+export class SubtracaoComponent {
+  private res : number = 0;
+
+  constructor(private subtracaoService: SubtracaoService) { }
+
+  subtrairBotao(numero1: string, numero2: string): void {
+    let n1: number;
+    let n2: number;
+    n1 = parseFloat(numero1);
+    n2 = parseFloat(numero2);
+    this.res = this.subtracaoService.subtrair(n1, n2);
+  }
+
+  get resultado(): string {
+    return this.res.toString();
+  }
+}
+
+//multiplicação
+import { Component } from '@angular/core';
+import { MultiplicacaoService } from '../../services';
+
+@Component({
+  selector: 'app-multiplicacao',
+  standalone: true,
+  imports: [],
+  templateUrl: './multiplicacao.component.html',
+  styleUrl: './multiplicacao.component.css'
+})
+export class MultiplicacaoComponent {
+  private res : number = 0;
+
+  constructor(private multiplicacaoService: MultiplicacaoService) { }
+
+  multiplicarBotao(numero1: string, numero2: string): void {
+    let n1: number;
+    let n2: number;
+    n1 = parseFloat(numero1);
+    n2 = parseFloat(numero2);
+    this.res = this.multiplicacaoService.multiplicar(n1, n2);
+  }
+
+  get resultado(): string {
+    return this.res.toString();
+  }
+}
+
+//divisão
+import { Component } from '@angular/core';
+import { DivisaoService } from '../../services';
+
+@Component({
+  selector: 'app-divisao',
+  standalone: true,
+  imports: [],
+  templateUrl: './divisao.component.html',
+  styleUrl: './divisao.component.css'
+})
+export class DivisaoComponent {
+  private res : number = 0;
+
+  constructor(private divisaoService: DivisaoService) { }
+
+  dividirBotao(numero1: string, numero2: string): void {
+    let n1: number;
+    let n2: number;
+    n1 = parseFloat(numero1);
+    n2 = parseFloat(numero2);
+    this.res = this.divisaoService.dividir(n1, n2);
+  }
+
+  get resultado(): string {
+    return this.res.toString();
+  }
+}
+
